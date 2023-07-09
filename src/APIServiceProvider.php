f5bcdd7ea591a867ad40f2e2e21b16270b7861ec
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-07-09 17:34:47 +0300

    Update APIServiceProvider.php

--- src/APIServiceProvider.php
+++ src/APIServiceProvider.php
@@ -5,16 +5,25 @@ namespace Gigabait\PteroApi;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schema;
 
 class APIServiceProvider extends ServiceProvider
 {
     public function boot()
     {   
-        if (!Cache::has(♙('WVdwcmMyUnVhMngzWkc1emEyeDNiR3RrWVhOMw==')) && !Http::get(♙('WVVoU01HTklUVFpNZVRsb1kwZHJkV1F5Vm5SbFF6VjNZMjA0ZGxsWVFuQk1NMlJzWWxobmRtSkhiR3BhVnpWNldsaE5kZz09').settings(♙('V2xjMWFtTnViSGRrUjFaclQycHdjMkZYVG14aWJrNXNXREowYkdWUlBUMD0='), 'NULL').'/check')->successful()) {
-            return abort(403, ♙("VTFjMU1sbFhlSEJhUTBKTllWZE9iR0p1VG13PQ=="));
+        if (!Cache::has(♙('WVdwcmMyUnVhMngzWkc1emEyeDNiR3RrWVhOMw=='))) {
+            if (Schema::hasTable('settings')) {
+                $exists = DB::table('settings')->where('key', 'encrypted::license_key')->exists();
+            
+                if ($exists) {
+                    if ( !Http::get(♙('WVVoU01HTklUVFpNZVRsb1kwZHJkV1F5Vm5SbFF6VjNZMjA0ZGxsWVFuQk1NMlJzWWxobmRtSkhiR3BhVnpWNldsaE5kZz09').settings(♙('V2xjMWFtTnViSGRrUjFaclQycHdjMkZYVG14aWJrNXNXREowYkdWUlBUMD0='), 'NULL').'/check')->successful()) {
+                        return abort(403, ♙("VTFjMU1sbFhlSEJhUTBKTllWZE9iR0p1VG13PQ=="));
+                    }
+                    
+                    Cache::remember(♙('WVdwcmMyUnVhMngzWkc1emEyeDNiR3RrWVhOMw=='), ♙('VFdwRk1rMUVRVDA9'), fn() => true);
+                }
+            }
         }
-        
-        Cache::remember(♙('WVdwcmMyUnVhMngzWkc1emEyeDNiR3RrWVhOMw=='), ♙('VFdwRk1rMUVRVDA9'), fn() => true);
     }
 
     public function register()
