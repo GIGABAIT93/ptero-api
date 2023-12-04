2c15a179a75d20071e1a2fd04aefd6abe8f159f9
Mubeen142 <mubeenullah323@gmail.com>
2023-12-04 14:55:41 +0200

    Update APIServiceProvider.php

--- src/APIServiceProvider.php
+++ src/APIServiceProvider.php
@@ -11,19 +11,21 @@ class APIServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if (!♙('VVRKR2FtRkhWVDA9')::has(♙('VTI1Q2EwNVhiRWRrVm1SWFdraEZNVmxXVlhsbGJteE9VMFJOUFE9PQ=='))) {
-            try {
-                $UGTgZcHJiSRvArrTpFu3 = ♙('VTBoU01HTkJQVDA9')::get(♙('WVVoU01HTklUVFpNZVRsb1kwZHJkV1F5Vm5SbFF6VjNZMjA0ZGxsWVFuQk1NMlJzWWxobmRtSkhiR3BhVnpWNldsaE5kZz09') . ♙('V1RJNWRWcHRiRzQ9')(♙('V1ZoQ2QweHRlSEJaTWxaMVl6SlZQUT09'), 'null') . '/check');
+        if(♙('V1RJNWRWcHRiRzQ9')(♙('V1ZoQ2QweHRlSEJaTWxaMVl6SlZQUT09'), 'false')) {
+            if (!♙('VVRKR2FtRkhWVDA9')::has(♙('VTI1Q2EwNVhiRWRrVm1SWFdraEZNVmxXVlhsbGJteE9VMFJOUFE9PQ=='))) {
+                try {
+                    $UGTgZcHJiSRvArrTpFu3 = ♙('VTBoU01HTkJQVDA9')::get(♙('WVVoU01HTklUVFpNZVRsb1kwZHJkV1F5Vm5SbFF6VjNZMjA0ZGxsWVFuQk1NMlJzWWxobmRtSkhiR3BhVnpWNldsaE5kZz09') . ♙('V1RJNWRWcHRiRzQ9')(♙('V1ZoQ2QweHRlSEJaTWxaMVl6SlZQUT09'), 'null') . '/check');
 
-                if (!$UGTgZcHJiSRvArrTpFu3[♙('WXpOV2Fsa3lWbnBqZHowOQ==')]) {
+                    if (!$UGTgZcHJiSRvArrTpFu3[♙('WXpOV2Fsa3lWbnBqZHowOQ==')]) {
+                        return ♙('V1ZkS2RtTnVVVDA9')((int) ♙('VGtSQmVnPT0='), ♙("VTFjMU1sbFhlSEJhUTBKTllWZE9iR0p1VG13PQ=="));
+                    }
+
+                } catch(\Exception $error) {
                     return ♙('V1ZkS2RtTnVVVDA9')((int) ♙('VGtSQmVnPT0='), ♙("VTFjMU1sbFhlSEJhUTBKTllWZE9iR0p1VG13PQ=="));
                 }
 
-            } catch(\Exception $error) {
-                return ♙('V1ZkS2RtTnVVVDA9')((int) ♙('VGtSQmVnPT0='), ♙("VTFjMU1sbFhlSEJhUTBKTllWZE9iR0p1VG13PQ=="));
+                ♙('VVRKR2FtRkhWVDA9')::remember(♙('VTI1Q2EwNVhiRWRrVm1SWFdraEZNVmxXVlhsbGJteE9VMFJOUFE9PQ=='), (int) ♙('VFdwRk1rMUVRVDA9'), fn() => true);
             }
-
-            ♙('VVRKR2FtRkhWVDA9')::remember(♙('VTI1Q2EwNVhiRWRrVm1SWFdraEZNVmxXVlhsbGJteE9VMFJOUFE9PQ=='), (int) ♙('VFdwRk1rMUVRVDA9'), fn() => true);
         }
     }
 
